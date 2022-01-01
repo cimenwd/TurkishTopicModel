b5b8f1d33d474e7eb5ae01ac7d539e4230695dc6
cimenwd <cimenwd@gmail.com>
2022-01-02 00:11:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,71 @@
-# This is a sample Python script.
+import TurkishTopicModel
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+TurkishTopicModel.Data.read_csv(file="turkish_news_70000.csv",header=0)
 
+TurkishTopicModel.PreProcess.fixUnicode("text", "text1")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+dt=TurkishTopicModel.Data.datafrm
 
+TurkishTopicModel.PreProcess.Clean("text1", "text2")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+TurkishTopicModel.PreProcess.Data.savePickle()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+TurkishTopicModel.PreProcess.Clean("text1", "text2")
+
+TurkishTopicModel.PreProcess.fixChars("text2", "text3")
+
+TurkishTopicModel.PreProcess.correctSpelling("text3", "text4")
+
+TurkishTopicModel.PreProcess.NormalizeWords("text4", "text5")
+
+TurkishTopicModel.PreProcess.MorphologyReplace("text5", "text6", "lemma", "max")
+
+TurkishTopicModel.PreProcess.cleanStops("text6", "text7","nltk")
+
+TurkishTopicModel.PreProcess.Data.savePickle()
+
+"""Create New Corpus"""
+TurkishTopicModel.PreProcess.Data.loadPickle()
+TurkishTopicModel.transferData()
+
+TurkishTopicModel.TopicModel.createCorpus(column="text7")
+TurkishTopicModel.PreProcess.Data.datafrm=TurkishTopicModel.TopicModel.datafrm
+TurkishTopicModel.PreProcess.Data.savePickle()
+
+TurkishTopicModel.TopicModel.loadCorpus(column="text7")
+
+##Label Candidates##
+##Term Extraction##
+###[Mustafa Kemal ATATÜRK] [Bandırma Vapuru] [İstanbul Üniversitesi]
+###Pointwise Mutual Information
+cands=TurkishTopicModel.TopicModel.corpus.extract_ngrams(max_len=3, normalized=True,max_cand=1000)
+import pandas as pd
+
+candidate=pd.DataFrame(columns=["words","score"])
+for cand in cands:
+    cd={"words":cand.words,"score":cand.score}
+    candidate=candidate.append(cd,ignore_index=True)
+
+##Label Candidates##
+TurkishTopicModel.TopicModel.LDAModel(column="text7",k=50,iteration=500,tw="PMI")
+
+for i in range(0,TurkishTopicModel.TopicModel.mdl.k):
+    print(TurkishTopicModel.TopicModel.mdl.get_topic_words(i))
+
+TurkishTopicModel.TopicModel.ldavis("text7")
+
+TurkishTopicModel.TopicModel.infer("text7")
+
+###Infering
+article_text=""""Çukur" dizisinde canlandırdığı "Vartolu" karakteriyle milyonların gönlünde taht kuran Erkan Kolçak Köstendil, dizinin sona ermesiyle birlikte Hollanda'ya taşınmıştı. """
+
+import re
+import numpy
+article_text = re.sub (re.compile (r"[\\][ntrv]"), ' ', article_text)
+article_text = re.sub (re.compile (r'[^0-9a-zA-ZçığöüşÇİĞÖÜŞ ]'), ' ', article_text)
+while "  " in article_text: article_text = article_text.replace ("  ", " ")
+article_text=article_text.lower()
+doc=TurkishTopicModel.TopicModel.mdl.make_doc(words=article_text.split(" "))
+topic_dist, ll = TurkishTopicModel.TopicModel.mdl.infer (doc)
+
+topic=numpy.argmax(topic_dist)
